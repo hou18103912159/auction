59612e21994caa9a35b9d3545f766f1fd3669011
hxh <18103912159@163.com>
2017-10-13 18:15:08 +0800

    20171013

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//装饰器
 @Component({
   selector: 'app-root',
+  //模板
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//控制器  一个简单的typescript类
 export class AppComponent {
   title = 'app';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
+//用来关闭angular的开发者模式
 import { enableProdMode } from '@angular/core';
+//使用哪个模块来启动整个应用
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+//主模块
 import { AppModule } from './app/app.module';
+//环境配置
 import { environment } from './environments/environment';
 
+//生产环境
 if (environment.production) {
   enableProdMode();
 }
 
+//启动应用
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
